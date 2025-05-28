018eefa063f34a8cf34cf56823ae8f14c09779f3
Kaushalya193 <e19193@eng.pdn.ac.lk>
2025-05-28 22:08:55 +0530

    Add routing setup and new components

---
+++ code/frontend/src/components/AdminDashboard.tsx
@@ -0,0 +1,10 @@
+
+export default function AdminDashboard() {
+  
+  return (
+    <div className="p-4">
+      <h2 className="text-xl mb-4">Admin Dashboard - Vote Counts</h2>
+        <p className="mb-4">Welcome to the Admin Dashboard. Here you can view the vote counts for each candidate.</p>
+    </div>
+  );
+}
---
+++ code/frontend/src/components/VoterDashboard.tsx
@@ -0,0 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
+export default function VoterDashboard() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl mb-4">Voter Dashboard</h2>
+        <p className="mb-4">Welcome to the Voter Dashboard. Here you can view candidates and cast your vote.</p>
+    </div>
+  );
+}

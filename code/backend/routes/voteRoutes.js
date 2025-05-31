c74fe12c63aa5016994747f02d99eb0d7958758e
Dasun <e19375@eng.pdn.ac.lk>
2025-05-31 17:44:19 +0530

    Deleting test files

--- code/backend/routes/voteRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const voteController = require('../controllers/voteController');
-
-router.post('/vote', voteController.castVote);
-router.get('/status/:id', voteController.checkStatus);
-
-module.exports = router;

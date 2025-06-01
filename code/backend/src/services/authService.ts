0b358cb0c203f3fe71565d7d26b699271e285828
Dasun <e19375@eng.pdn.ac.lk>
2025-06-01 11:23:48 +0530

    Fixing auth service

--- code/backend/src/services/authService.ts
+++ code/backend/src/services/authService.ts
@@ -1,7 +1,19 @@
 import bcrypt from "bcrypt";
+import { dbQuery } from "./dbService";
 
 export const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt();
   const hashedPassword = await bcrypt.hash(password, salt);
   return hashedPassword;
+};
+
+export const validateUserName = async (username: string): Promise<boolean> => {
+  const currentUsernames: { user_name: string }[] = (
+    await dbQuery({ query: "SELECT user_name FROM admin_data", params: [] })
+  ).rows;
+  const isDuplicate: boolean = currentUsernames.some(
+    (name) => name.user_name === username
+  );
+
+  return isDuplicate;
 };

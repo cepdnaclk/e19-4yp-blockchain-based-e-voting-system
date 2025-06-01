3798aed8b17fa96f89b929ef11d9d3c149103b2a
Kaushalya193 <e19193@eng.pdn.ac.lk>
2025-06-02 01:29:35 +0530

    Update voter login routes

--- code/backend/src/routes/voterLoginRoutes.ts
+++ code/backend/src/routes/voterLoginRoutes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { voterLogin } from '../controllers/voterLoginController';
+import { registerVoter, voterLogin } from '../controllers/voterLoginController';
 
 const router = Router();
 
 router.post('/userlogin', (req, res) => {
   voterLogin(req, res);
+});
+
+router.post('/register', (req, res) => {
+  registerVoter(req, res); 
 });
 
 export default router; 

1e618fd1cff539fb1961f02c7e9f71b7a3e973b5
Dasun <e19375@eng.pdn.ac.lk>
2025-06-01 11:44:18 +0530

    Login routes

--- code/backend/src/routes/loginRouter.ts
+++ code/backend/src/routes/loginRouter.ts
@@ -1,16 +1,36 @@
 import express, { Router } from "express";
+import { validatePasswrd } from "../services/authService";
+import { dbQuery } from "../services/dbService";
 
 const router: Router = express.Router();
 
 // Login route
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
+  const user: {
+    id: string;
+    user_name: string;
+    password: string;
+    created_at: Date;
+  }[] = (
+    await dbQuery({
+      query: "SELECT * FROM admin_data WHERE user_name = $1",
+      params: [username],
+    })
+  ).rows;
 
-  // Dummy authentication logic
-  if (username === "admin" && password === "password") {
-    res.status(200).json({ message: "Login successful", token: "dummy-token" });
+  if (user.length === 0) {
+    res.status(400).json({ message: "User not found", userName: username });
+    return;
+  }
+
+  const isPasswordMatch = await validatePasswrd(password, user[0].password);
+  if (isPasswordMatch) {
+    res.status(200).json({ message: "Authenticated", userName: username });
   } else {
-    res.status(401).json({ error: "Invalid credentials" });
+    res
+      .status(200)
+      .json({ message: "Password does not match", userName: username });
   }
 });
 

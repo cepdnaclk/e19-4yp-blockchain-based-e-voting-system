d5e1a8ff05303ec7c30bc08dc518f95719f4959b
Dasun <e19375@eng.pdn.ac.lk>
2025-06-01 11:24:48 +0530

    Migration file to make the user-name unique

---
+++ code/backend/migrations/1748754336698_adding-unique-to-user-name.mjs
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addConstraint("admin_data", "unique_user_name", {
+    unique: ["user_name"],
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropConstraint("admin_data", "unique_user_name");
+};

8c2b26797a9a7b39adc38e2ba9f85710ebbb0f86
Dasun <e19375@eng.pdn.ac.lk>
2025-06-03 01:25:39 +0530

    Main and router files

--- code/frontend/src/router.tsx
+++ code/frontend/src/router.tsx
@@ -1,27 +1,27 @@
-
-import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import UserLogin from "./components/UserLogin";
+import AdminDashboard from "./components/AdminDashboard";
+import AdminForgotPassword from "./components/AdminForgotPassword";
 import AdminLogin from "./components/AdminLogin";
 import AdminOTP from "./components/AdminOTP";
 import Layout from "./components/Layout";
+import NotFound from "./components/NotFound";
 import UserDashboard from "./components/UserDashboard";
-import AdminDashboard from "./components/AdminDashboard";
-import AdminForgotPassword from './components/AdminForgotPassword';
+import UserLogin from "./components/UserLogin";
+import Unauthorized from "./components/Unauthorized";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-
       { path: "", element: <UserLogin /> },
       { path: "user/dashboard", element: <UserDashboard /> },
       { path: "admin/login", element: <AdminLogin /> },
-      { path: 'admin/forgot-password', element: <AdminForgotPassword /> },
+      { path: "admin/forgot-password", element: <AdminForgotPassword /> },
       { path: "admin/otp", element: <AdminOTP /> },
       { path: "admin/dashboard", element: <AdminDashboard /> },
-
+      { path: "unautharized", element: <Unauthorized /> },
+      { path: "*", element: <NotFound /> },
     ],
   },
 ]);

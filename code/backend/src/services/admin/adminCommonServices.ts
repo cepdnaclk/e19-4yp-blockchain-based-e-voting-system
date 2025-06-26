76933fc5ee592e6aefe1720a74783805f2bfc0e2
Dasun <e19375@eng.pdn.ac.lk>
2025-06-27 01:18:39 +0530

    Adding admin common service and admin registration service

---
+++ code/backend/src/services/admin/adminCommonServices.ts
@@ -0,0 +1,14 @@
+import { AdminUserType } from "../../common/types/adminTypes";
+import { dbQuery } from "../dbService";
+
+export const getAdminUsersByUsername = async (
+  username: string
+): Promise<AdminUserType[]> => {
+  const results: AdminUserType[] = (
+    await dbQuery({
+      query: "SELECT * FROM admin_data WHERE user_name = $1",
+      params: [username],
+    })
+  ).rows;
+  return results;
+};
--- code/backend/src/services/admin/adminRegisterService.ts
+++ code/backend/src/services/admin/adminRegisterService.ts
@@ -1,10 +1,10 @@
 import { Response } from "express";
-import { sendSuccess } from "../../utils/responseHandler";
+import { sendError, sendSuccess } from "../../utils/responseHandler";
 import { hashPassword } from "../authService";
 import { dbQuery } from "../dbService";
-import messages from "../../constants/messages";
+import messages from "../../common/constants/messages";
 
-const adminRegisterService = async (
+export const adminRegisterService = async (
   res: Response,
   username: string,
   password: string
@@ -25,4 +25,29 @@ const adminRegisterService = async (
   });
 };
 
-export default adminRegisterService;
+export const addRefreshTokenToDb = async (
+  res: Response,
+  username: string,
+  accessToken: string,
+  refreshToken: string
+) => {
+  const query = "UPDATE admin_data SET refresh_token = $1 WHERE user_name = $2";
+  const param = [refreshToken, username];
+  try {
+    const response = await dbQuery({ query: query, params: param });
+    res
+      .status(200)
+      .cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+      })
+      .json({
+        message: "Authenticated",
+        userName: username,
+        accessToken: accessToken,
+      });
+  } catch {
+    sendError(res, 500, { message: messages.common.error });
+  }
+};

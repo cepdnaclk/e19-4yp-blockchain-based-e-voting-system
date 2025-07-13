dcb1efb8b1569be6d783a20532ac88696c24ec0c
Dasun <e19375@eng.pdn.ac.lk>
2025-07-14 02:00:50 +0530

    Admin register and logig related changes

--- code/backend/src/services/admin/adminRegisterService.ts
+++ code/backend/src/services/admin/adminRegisterService.ts
@@ -1,22 +1,24 @@
+import { blockchainResponseType } from "../../common/types/blockchainResponseTypes";
 import { hashPassword } from "../auth/authService";
 import { blockchainPostPut } from "../blockchain/blockchainServices";
 
 export const adminRegisterService = async (
   username: string,
   password: string
-): Promise<{ id: number }> => {
+): Promise<blockchainResponseType> => {
   const hashedPassword = await hashPassword(password);
-  const response = await blockchainPostPut(
+  const response: blockchainResponseType = await blockchainPostPut(
     new Map([
       [
         "admin",
-        JSON.stringify({ username: "admin", hashedPassword: hashedPassword }),
+        JSON.stringify({
+          username: username,
+          hashedPassword: hashedPassword.toString(),
+        }),
       ],
     ]),
     false
   );
 
-  console.log(response);
-
-  return { id: 0 };
+  return response;
 };
